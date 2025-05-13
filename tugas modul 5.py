708307f1fb7bf77c13216a103124a1bc061deabd
AISYAHASHARI10 <aisyahashari83@gmail.com>
2025-05-13 21:05:24 +0700

    Add files via upload

---
+++ tugas modul 5.py
@@ -0,0 +1,11 @@
+nilai_x = [-7,-6,-5,-4,-3,-2,-1,0,11,2,3,4,5,6,7]
+fungsi_f = []
+for x in range(-7,8) :
+    if x > 0 :
+        fungsi_f = x^3 -x
+    if x < 0 :
+        fungsi_f = 1//x^2
+    if x < 0 :
+        fungsi_f = 1
+print("|  x  |  f(x)  |")
+print(nilai_x{0})
