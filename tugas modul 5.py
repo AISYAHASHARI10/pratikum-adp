90ac170bafa971108e9d6402c59784953f3d2211
AISYAHASHARI10 <aisyahashari83@gmail.com>
2025-05-13 21:06:24 +0700

    Update tugas modul 5.py

--- tugas modul 5.py
+++ tugas modul 5.py
@@ -1,11 +1,28 @@
-nilai_x = [-7,-6,-5,-4,-3,-2,-1,0,11,2,3,4,5,6,7]
-fungsi_f = []
-for x in range(-7,8) :
-    if x > 0 :
-        fungsi_f = x^3 -x
-    if x < 0 :
-        fungsi_f = 1//x^2
-    if x < 0 :
-        fungsi_f = 1
-print("|  x  |  f(x)  |")
-print(nilai_x{0})
+nilai_x = [-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7]
+nilai_fx = []
+for x in nilai_x:
+    if x > 0:
+        fx = x**3 - x
+    elif x < 0:
+        fx = 1 / (x**2) 
+    else:
+        fx = 1 
+    nilai_fx.append(fx)
+print("|   x   |          f(x)          |")
+batas = len(nilai_x)
+for i in range(batas,batas + 1):
+ print("|  " ,nilai_x[0] ," | " ,nilai_fx[0], "  |" )
+ print("|  " ,nilai_x[1] ," | " ,nilai_fx[1], " |" )
+ print("|  " ,nilai_x[2] ," | " ,nilai_fx[2], "                 |" )
+ print("|  " ,nilai_x[3] ," | " ,nilai_fx[3], "               |" )
+ print("|  " ,nilai_x[4] ," | " ,nilai_fx[4], "   |" )
+ print("|  " ,nilai_x[5] ," | " ,nilai_fx[5], "                 |" )
+ print("|  " ,nilai_x[6] ," | " ,nilai_fx[6], "                  |" )
+ print("|   " ,nilai_x[7] ," | " ,nilai_fx[7], "                    |" )
+ print("|   " ,nilai_x[8] ," | " ,nilai_fx[8], "                    |" )
+ print("|   " ,nilai_x[9] ," | " ,nilai_fx[9], "                    |" )
+ print("|   " ,nilai_x[10] ," | " ,nilai_fx[10], "                   |" )
+ print("|   " ,nilai_x[11] ," | " ,nilai_fx[11], "                   |" )
+ print("|   " ,nilai_x[12] ," | " ,nilai_fx[12], "                  |" )
+ print("|   " ,nilai_x[13] ," | " ,nilai_fx[13], "                  |" )
+ print("|   " ,nilai_x[14] ," | " ,nilai_fx[14], "                  |" )
