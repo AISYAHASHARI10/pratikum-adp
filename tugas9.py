64c33729087eb32ef7a69b262afbf5a0848acf3a
AISYAHASHARI10 <aisyahashari83@gmail.com>
2025-06-10 16:16:08 +0700

    Add files via upload

---
+++ tugas9.py
@@ -0,0 +1,19 @@
+import time
+import os
+from termcolor import colored 
+pesan = "    HAPPY EID  "
+lebar = 15
+total_langkah = 20
+delay = 1
+
+
+pesan_panjang = pesan * 5
+
+# Animasi scroll
+i = 0
+while i < total_langkah:
+    os.system('cls')  
+    potongan = pesan_panjang[i:i+lebar]  
+    print(colored(potongan, 'black', 'on_yellow'))  
+    time.sleep(delay)
+    i = i + 1
